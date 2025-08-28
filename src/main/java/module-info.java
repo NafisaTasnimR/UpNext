04c0f00cfde38996efb0e9e64f3ef9f89d2f05ca
nafisa <nafisatasnim8855@gmail.com>
2025-08-28 20:13:22 +0600

    Update structural file
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.upnext {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires com.oracle.database.jdbc;
 
 
     opens org.example.upnext to javafx.fxml;
